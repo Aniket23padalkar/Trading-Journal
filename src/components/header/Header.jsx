6ad27938d813bce43cc4c6c67a882fa5a58cc9b3
Aniket Padalkar <aniket23.padalkar@gamil.com>
2025-10-21 18:35:41 +0530

    updated logo location and route in app.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,22 @@
 import { Link } from "react-router-dom";
 import "./header.css";
+import logo from "../../src/assets/TradeLens-Logo2.png";
 
 export default function Header() {
-    return (
-        <header>
-            <div className="header-left">
-                <Link to="/">
-                    <div className="logo">
-                        <img
-                            src="../../src/assets/TradeLens-Logo2.png"
-                            alt="Logo"
-                        />
-                    </div>
-                </Link>
-            </div>
-            <div className="header-right">
-                <div className="welcome-msg">
-                    <h4>Hello, Aniket</h4>
-                </div>
-            </div>
-        </header>
-    );
+  return (
+    <header>
+      <div className="header-left">
+        <Link to="/">
+          <div className="logo">
+            <img src={logo} alt="Logo" />
+          </div>
+        </Link>
+      </div>
+      <div className="header-right">
+        <div className="welcome-msg">
+          <h4>Hello, Aniket</h4>
+        </div>
+      </div>
+    </header>
+  );
 }
