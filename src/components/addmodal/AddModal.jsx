1b14de1ba661b3982d6fec778bb2ee494a53d6d4
Aniket Padalkar <aniket23.padalkar@gmail.com>
2025-10-21 18:15:07 +0530

    initial-commit

---
+++ src/components/addmodal/AddModal.jsx
@@ -0,0 +1,263 @@
+import { useContext } from "react";
+import "./addmodal.css";
+import { GlobalContext } from "../../context/Context";
+
+export default function AddModal() {
+  const {
+    trades,
+    setTrades,
+    setAddModal,
+    formData,
+    setFormData,
+    currentEditId,
+    calculatePnL,
+    calculateRRratio,
+  } = useContext(GlobalContext);
+
+  const orderSideColor = () => {
+    if (formData.order === "BUY") return "#48e200ff";
+    if (formData.order === "SELL") return "#ff5659ff";
+    return "white";
+  };
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }
+
+  function resetForm() {
+    setFormData({
+      symbol: "",
+      order: "",
+      status: "",
+      buyPrice: "",
+      sellPrice: "",
+      marketType: "",
+      quantity: "",
+      risk: "",
+      position: "",
+      entryTime: "",
+      exitTime: "",
+      rating: "",
+      description: "",
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const pnl = calculatePnL(
+      formData.buyPrice,
+      formData.sellPrice,
+      formData.quantity
+    );
+
+    const rrRatio = calculateRRratio(formData);
+
+    if (currentEditId) {
+      const updatedTrade = trades.map((trade) =>
+        trade.id === currentEditId
+          ? { ...trade, formData: { ...formData }, pnl, rrRatio }
+          : trade
+      );
+      setTrades(updatedTrade);
+      resetForm();
+    } else {
+      setTrades((prev) => [
+        ...prev,
+        { formData, pnl, rrRatio, id: Date.now() },
+      ]);
+      resetForm();
+    }
+
+    setAddModal(false);
+  }
+
+  function handleCloseModal() {
+    setAddModal(false);
+    resetForm();
+  }
+
+  return (
+    <div className="add-modal-container">
+      <div className="add-modal">
+        <div className="add-modal-header">
+          <h1>Input Trade Details</h1>
+          <button onClick={handleCloseModal} id="close-add-modal">
+            X
+          </button>
+        </div>
+        <form onSubmit={handleSubmit} className="input-form">
+          <div className="first-row">
+            <input
+              name="symbol"
+              value={formData.symbol}
+              required
+              className="input"
+              type="text"
+              placeholder="Symbol"
+              onChange={handleChange}
+            />
+            <div className="select-container">
+              <select
+                required
+                value={formData.order}
+                name="order"
+                className="select"
+                onChange={handleChange}
+                style={{ backgroundColor: orderSideColor() }}
+              >
+                <option value="">Order</option>
+                <option value="BUY">BUY</option>
+                <option value="SELL">SELL</option>
+              </select>
+              <select
+                onChange={handleChange}
+                value={formData.status}
+                required
+                name="status"
+                className="select"
+              >
+                <option value="">Status</option>
+                <option value="Open">Open</option>
+                <option value="Closed">Closed</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="second-row">
+            <div className="price">
+              <input
+                value={formData.buyPrice}
+                name="buyPrice"
+                required
+                disabled={
+                  formData.status === "Open" && formData.order === "SELL"
+                }
+                className="price-input"
+                type="number"
+                placeholder="Buy Price"
+                onChange={handleChange}
+              />
+              <input
+                value={formData.sellPrice}
+                name="sellPrice"
+                required
+                disabled={
+                  formData.status === "Open" && formData.order === "BUY"
+                }
+                className="price-input"
+                type="number"
+                placeholder="Sell Price"
+                onChange={handleChange}
+              />
+            </div>
+            <select
+              value={formData.marketType}
+              required
+              name="marketType"
+              className="select market-type"
+              onChange={handleChange}
+            >
+              <option value="">Market-Type</option>
+              <option value="Equity">Equity</option>
+              <option value="Options">Options</option>
+              <option value="Futures">Futures</option>
+            </select>
+          </div>
+
+          <div className="third-row">
+            <input
+              required
+              value={formData.quantity}
+              name="quantity"
+              type="number"
+              placeholder="quantity"
+              className="price-input"
+              onChange={handleChange}
+            />
+            <input
+              required
+              value={formData.risk}
+              name="risk"
+              type="number"
+              placeholder="Risk"
+              className="price-input"
+              onChange={handleChange}
+            />
+            <select
+              required
+              value={formData.position}
+              name="position"
+              onChange={handleChange}
+              className="select market-type"
+            >
+              <option value="">Position</option>
+              <option value="Intraday">Intraday</option>
+              <option value="BTST">BTST</option>
+              <option value="STBT">STBT</option>
+              <option value="Swing">Swing</option>
+              <option value="Positional">Positional</option>
+              <option value="Long-Term">Long-Term</option>
+            </select>
+          </div>
+
+          <div className="fourth-row">
+            <div className="entry-time">
+              <label>Entry Time:</label>
+              <input
+                value={formData.entryTime}
+                name="entryTime"
+                required
+                type="datetime-local"
+                className="price-input date-time"
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="exit-time">
+              <label>Exit Time:</label>
+              <input
+                value={formData.exitTime}
+                name="exitTime"
+                required
+                disabled={formData.status === "Open"}
+                type="datetime-local"
+                className="price-input date-time"
+                onChange={handleChange}
+              />
+            </div>
+
+            <select
+              value={formData.rating}
+              style={{ marginTop: "1rem" }}
+              required
+              name="rating"
+              className="select market-type"
+              onChange={handleChange}
+            >
+              <option value="">Trade-Rate</option>
+              <option value="Worst">Worst</option>
+              <option value="Poor">Poor</option>
+              <option value="Average">Average</option>
+              <option value="Good">Good</option>
+              <option value="Best">Best</option>
+            </select>
+          </div>
+
+          <textarea
+            value={formData.description}
+            name="description"
+            id="description"
+            placeholder="Describe this Trade...."
+            onChange={handleChange}
+          ></textarea>
+          <button id="save-btn">Save</button>
+        </form>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/viewmodal/ViewModal.jsx
@@ -0,0 +1,118 @@
+import { useContext } from "react";
+import "./viewmodal.css";
+import { GlobalContext } from "../../context/Context";
+
+export default function ViewModal() {
+  const { setViewModal, currentViewTrade, formatDateTime } =
+    useContext(GlobalContext);
+
+  function handleViewOrderColor() {
+    if (currentViewTrade.formData.order === "BUY") return "#6fca44ff";
+    if (currentViewTrade.formData.order === "SELL") return "#fc4a4dff";
+    return "white";
+  }
+
+  function handleRatingColor() {
+    if (currentViewTrade.formData.rating === "Worst") return "red";
+    if (currentViewTrade.formData.rating === "Poor") return "#ff787aff";
+    if (currentViewTrade.formData.rating === "Average") return "#000000ff";
+    if (currentViewTrade.formData.rating === "Good") return "#66c43bff";
+    if (currentViewTrade.formData.rating === "Best") return "green";
+    return "black";
+  }
+
+  return (
+    <div className="view-modal-container">
+      <div className="view-modal">
+        <div className="view-modal-header">
+          <h1>{currentViewTrade.formData.symbol}</h1>
+          <span
+            style={{ backgroundColor: handleViewOrderColor(), color: "white" }}
+          >
+            {currentViewTrade.formData.order}
+          </span>
+        </div>
+        <div className="view-modal-content">
+          <div className="view-content-container">
+            <div className="section">
+              <span>Buy-Price</span>
+              <h1>{currentViewTrade.formData.buyPrice}</h1>
+            </div>
+            <div className="section">
+              <span>Sell-Price</span>
+              <h1>{currentViewTrade.formData.sellPrice}</h1>
+            </div>
+            <div className="section">
+              <span>Status</span>
+              <h1>{currentViewTrade.formData.status}</h1>
+            </div>
+            <div className="section">
+              <span>Market-Type</span>
+              <h1>{currentViewTrade.formData.marketType}</h1>
+            </div>
+          </div>
+
+          <div className="view-content-container">
+            <div className="section">
+              <span>Quantity</span>
+              <h1>{currentViewTrade.formData.quantity}</h1>
+            </div>
+            <div className="section">
+              <span>Risk</span>
+              <h1>{currentViewTrade.formData.risk}/-</h1>
+            </div>
+            <div className="section">
+              <span>Position</span>
+              <h1>{currentViewTrade.formData.position}</h1>
+            </div>
+            <div className="section">
+              <span>Trade-Rating</span>
+              <h1 style={{ color: handleRatingColor() }}>
+                {currentViewTrade.formData.rating}
+              </h1>
+            </div>
+          </div>
+
+          <div className="view-content-container">
+            <div className="section">
+              <span>Entry-Time</span>
+              <h1 style={{ color: "blue" }}>
+                {formatDateTime(currentViewTrade.formData.entryTime)}
+              </h1>
+            </div>
+            <div className="section">
+              <span>PnL</span>
+              <h1
+                style={{
+                  color: currentViewTrade.pnl >= 0 ? "green" : "red",
+                  fontSize: "1rem",
+                }}
+              >
+                {currentViewTrade.pnl}
+              </h1>
+            </div>
+            <div className="section">
+              <span>R:R Ratio</span>
+              <h1>{currentViewTrade.rrRatio}X</h1>
+            </div>
+            <div className="section">
+              <span>Exit-Time</span>
+              <h1 style={{ color: "blue" }}>
+                {formatDateTime(currentViewTrade.formData.exitTime)}
+              </h1>
+            </div>
+          </div>
+          <div className="trade-details">
+            <span>
+              <h1>Trade-Details : </h1>
+              {currentViewTrade.formData.description}
+            </span>
+          </div>
+        </div>
+        <div className="close-view-modal">
+          <button onClick={() => setViewModal(false)}>Close</button>
+        </div>
+      </div>
+    </div>
+  );
+}
