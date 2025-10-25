5c225b21e636755a75302678574fe765c40186d4
Aniket Padalkar <aniket23.padalkar@gamil.com>
2025-10-25 10:57:47 +0530

    updated UI

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/Trading-Journal/",
+  server: {
+    host: true, // allows access from network & ngrok
+    port: 5173,
+    strictPort: true,
+    allowedHosts: [
+      "uninthralled-gropingly-crew.ngrok-free.dev",
+      "localhost",
+      "127.0.0.1",
+    ],
+  },
 });
