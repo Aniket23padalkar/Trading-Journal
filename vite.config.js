ba94b492e596d7a31d7f1e70a4af43e3c7cb2034
Aniket Padalkar <aniket23.padalkar@gmail.com>
2025-10-26 21:50:06 +0530

    deploying to vercel

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Trading-Journal/",
+  base: process.env.VITE_BASE_PATH || "/Trading-Journal",
   server: {
     host: true, // allows access from network & ngrok
     port: 5173,
