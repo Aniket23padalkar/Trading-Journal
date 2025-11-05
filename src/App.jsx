e10243520a8e1910d0704dc89727b729c8142a67
Aniket Padalkar <aniket23.padalkar@gmail.com>
2025-11-05 10:09:15 +0530

    adjusted some fitting issue

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,12 @@ import ContactUs from "./pages/ContactUs";
 export default function App() {
   return (
     <>
-      <main className="grid grid-cols-[14rem_1fr] grid-rows-[4rem_1fr] h-full bg-white dark:bg-gray-950">
+      <main className="grid grid-cols-[14rem_1fr] grid-rows-[4rem_1fr] bg-white dark:bg-gray-950">
         <Header />
 
         <Aside />
 
-        <div className="lg:col-start-2 lg:col-end-3 col-start-1 col-end-3 row-start-2 m-2 lg:mr-4 mb-4 bg-gray-100 dark:bg-gray-800 rounded-3xl">
+        <div className="lg:col-start-2 lg:col-end-3 col-start-1 col-end-3 row-start-2 lg:mx-4 mb-4 bg-gray-100 dark:bg-gray-800 rounded-3xl">
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/trades" element={<Trades />} />
