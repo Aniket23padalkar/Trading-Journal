b1d5d409a97c6021c37b95c50539598cc9785574
Aniket Padalkar <aniket23.padalkar@gmail.com>
2026-04-01 08:57:05 +0530

    updated database deployment

--- backend/config/auth.js
+++ backend/config/auth.js
@@ -4,11 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USERNAME,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.on("connect", () => {
