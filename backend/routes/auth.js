f2861b1b25286e3a320ac628d661f7abfbf2b78e
Aniket Padalkar <aniket23.padalkar@gmail.com>
2026-04-01 16:50:35 +0530

    added supabase url

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,8 +8,8 @@ const router = express.Router();
 
 const cookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: "strict",
+  secure: true,
+  sameSite: "none",
   maxAge: 30 * 24 * 60 * 60 * 1000, // 30days
 };
 
