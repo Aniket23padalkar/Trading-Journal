cd11d50b0470ac51dfaf9305913b043954f348c7
Aniket Padalkar <aniket23.padalkar@gmail.com>
2026-06-07 14:55:30 +0530

    modified global express request added validated object

--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -4,6 +4,11 @@ declare global {
   namespace Express {
     interface Request {
       user: SafeUser;
+      validated?: {
+        body?: any;
+        params?: any;
+        query?: any;
+      };
     }
   }
 }
