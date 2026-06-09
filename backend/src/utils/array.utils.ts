aa88c799129770019708927ea22071d03895c1a0
Aniket23padalkar <aniket.23padalkar@gmail.com>
2026-06-09 09:27:11 +0530

    created a reusable reduce function for mapping

--- backend/src/utils/array.utils.ts
+++ backend/src/utils/array.utils.ts
@@ -17,3 +17,19 @@ export function groupBy<T, K extends keyof T>(
     {} as Record<string, T[]>,
   );
 }
+
+export function mapBy<T, K extends keyof T>(
+  array: T[],
+  key: K,
+): Record<string, T> {
+  return array.reduce(
+    (acc, item) => {
+      const mapKey = String(item[key]);
+
+      acc[mapKey] = item;
+
+      return acc;
+    },
+    {} as Record<string, T>,
+  );
+}
