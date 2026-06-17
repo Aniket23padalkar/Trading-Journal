3f0f009eb49a6df406818862e2954e79f8587c02
Aniket23padalkar <aniket.23padalkar@gmail.com>
2026-06-17 10:44:59 +0530

    Converted protected layouts to tsx

--- frontend/src/layouts/ProtectedRoute.tsx
+++ frontend/src/layouts/ProtectedRoute.tsx
@@ -1,10 +1,9 @@
 import { Navigate } from "react-router-dom";
-import { useContext, type ReactNode } from "react";
-import { AuthContext } from "../context/AuthContext.js";
 import { ScaleLoader } from "react-spinners";
 import { useAuthContext } from "../hooks/useAuthContext.js";
+import type { ContextProviderProps } from "../types/context.types.js";
 
-export default function ProtectedRoute({ children }: React.ReactNode) {
+export default function ProtectedRoute({ children }: ContextProviderProps) {
   const { user, authLoading } = useAuthContext();
 
   if (authLoading)
